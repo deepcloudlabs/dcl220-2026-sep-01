43cffad14c7fde4b3ebb63c9c106d4b1e42416a2
Binnur KURT <deepcloudlabs@gmail.com>
2026-09-03 11:24:32 +0300

    Update Exercise02.java

--- study-reactive-programming/src/com/example/exercises/Exercise02.java
+++ study-reactive-programming/src/com/example/exercises/Exercise02.java
@@ -1,38 +1,59 @@
 package com.example.exercises;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class Exercise02 {
 
 	public static void main(String[] args) {
-		Job job = new Job();
-		var syncResult = job.fun();
-		System.out.println("Sync Result is %d.".formatted(syncResult));		
-		job.gun().thenAcceptAsync( asyncResult -> {
-			System.out.println("Async Result is %d.".formatted(asyncResult));
-		});
-		for (var i=0;i<1_000;++i) {
-			try {TimeUnit.MILLISECONDS.sleep(10);}catch(Exception e) {}
-			System.out.println("Application is processing data [%d]".formatted(i));
+		try (var tp = Executors.newSingleThreadExecutor(); Job job = new Job();) {
+
+			var syncResult = job.fun();
+			System.out.println("Sync Result is %d.".formatted(syncResult));
+			job.gun().thenAcceptAsync(asyncResult -> {
+				System.out.println("[%s] Async Result is %d.".formatted(Thread.currentThread().getName(), asyncResult));
+			}, tp);
+			for (var i = 0; i < 1_000; ++i) {
+				try {
+					TimeUnit.MILLISECONDS.sleep(10);
+				} catch (Exception e) {
+				}
+				System.out.println("Application is processing data [%d]".formatted(i));
+			}
+		} catch (Exception e1) {
+			e1.printStackTrace();
 		}
 	}
 
 }
 
-class Job {
-	
+class Job implements AutoCloseable {
+	private ExecutorService tp = Executors.newFixedThreadPool(20);
+
 	public int fun() { // sync -> blocking
-		try {TimeUnit.SECONDS.sleep(5);}catch(Exception e) {}
+		try {
+			TimeUnit.SECONDS.sleep(5);
+		} catch (Exception e) {
+		}
 		return 42;
 	}
-	
+
 	public CompletableFuture<Integer> gun() { // async -> non-blocking
 		return CompletableFuture.supplyAsync(() -> {
-			try {TimeUnit.SECONDS.sleep(5);}catch(Exception e) {}
-			return 42;			
-		});
+			try {
+				TimeUnit.SECONDS.sleep(5);
+			} catch (Exception e) {
+			}
+			return 42;
+		}, tp);
 	}
-	
-	
+
+	@Override
+	public void close() throws Exception {
+		tp.shutdown();
+
+	}
+
 }
